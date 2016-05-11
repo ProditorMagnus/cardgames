043f29a95a1aab18f39dd8a8102906219d669d19
ProditorMagnus <public.ravana@gmail.com>
2016-05-11 18:06:16 +0300

    event skeleton

--- bjdisplay.cpp
+++ bjdisplay.cpp
@@ -1,6 +1,22 @@
 #include "bjdisplay.h"
 
-BJdisplay::BJdisplay()
+BJdisplay::BJdisplay(QWidget *parent)
+    : QWidget(parent)
 {
+    // Nothing here yet
+    setMouseTracking(true);
+}
+
+void BJdisplay::paintEvent(QPaintEvent *event) {
+    QPainter painter(this);
+}
+
+void BJdisplay::mousePressEvent(QMouseEvent *event){
+
+}
+void BJdisplay::mouseReleaseEvent(QMouseEvent *event){
+
+}
+void BJdisplay::mouseMoveEvent(QMouseEvent *event){
 
 }
--- bjdisplay.h
+++ bjdisplay.h
@@ -1,11 +1,25 @@
 #ifndef BJDISPLAY_H
 #define BJDISPLAY_H
+#include <QPainter>
+#include <QPaintEvent>
+#include <QWidget>
+#include <QAction>
+#include <QApplication>
+#include <QEvent>
+#include <QMenu>
+#include <QMenuBar>
+#include <QStatusBar>
+#include <QWhatsThis>
 
-
-class BJdisplay
+class BJdisplay : public QWidget
 {
+    Q_OBJECT
 public:
-    BJdisplay();
+    BJdisplay(QWidget *parent = 0);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void paintEvent(QPaintEvent *event);
 };
 
 #endif // BJDISPLAY_H
