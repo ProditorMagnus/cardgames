6939a8c95e1f5ca3fa81c33776b7bafc8991b65f
ProditorMagnus <public.ravana@gmail.com>
2016-05-12 20:55:43 +0300

    questions comment

--- main.cpp
+++ main.cpp
@@ -13,3 +13,5 @@ int main(int argc, char *argv[])
 // https://code.google.com/archive/p/vector-playing-cards/
 // kasutab boosti
 // kas diiler näeb mis kaardid mängija võttis
+// kui on äss, 4, äss, emand, summa 16, või 26, või 36, kas see on >=17 nagu vaja?
+// ning iga kord kui on äss, mida kasutatakse kui väiksemat et ei läheks üle 21
